24d86cfc4c750cf9fc400d38a578c918a8072449
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-03-07 23:21:38 +0900

    [update] バージョン2の開発開始

---
+++ Assets/Reuse Components/PlayerController2D.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
