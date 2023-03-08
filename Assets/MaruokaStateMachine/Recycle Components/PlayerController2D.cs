686712fea165350bfc72a6ba04fdaec2220fc171
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-03-08 16:41:25 +0900

    [update] コメントの追記とフォルダの整理とコードの修正を実施

--- Assets/MaruokaStateMachine/Recycle Components/PlayerController2D.cs
+++ Assets/MaruokaStateMachine/Recycle Components/PlayerController2D.cs
@@ -11,6 +11,8 @@ public class PlayerController2D : MonoBehaviour
     private float _groundedLayLength = 1.3f;
     [SerializeField]
     private LayerMask _layerMask = default;
+    [SerializeField]
+    private SelfMadeStateMachine _stateMachine = default;
 
     private Rigidbody2D _rigidbody2D = null;
 
@@ -20,28 +22,26 @@ public class PlayerController2D : MonoBehaviour
         _rigidbody2D.freezeRotation = true;
     }
 
-    [SerializeField]
-    private MySelfStateMachine _stateMachine = default;
-
     private void Update()
     {
+        // 移動とジャンプの処理
         var h = Input.GetAxisRaw("Horizontal") * _moveSpeed;
         var v = _rigidbody2D.velocity.y;
 
         RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, _groundedLayLength, _layerMask);
 
         if (hit.collider != null &&
-            Input.GetButtonDown("Jump"))
+            Input.GetKeyDown(KeyCode.Space))
         {
             v = _jumpPower;
         }
 
         _rigidbody2D.velocity = new Vector2(h, v);
 
-        // ================================================= //
+        // ステートマシンの状態変更
         if (hit.collider == null)
         {
-            _stateMachine.Conditions = Conditions.Midair;
+            _stateMachine.Conditions = Conditions.Jump;
         }
         else if (Mathf.Abs(_rigidbody2D.velocity.x) > 0.01f)
         {
